d9fdc5a7335024b0d245ba7d7b3af060def4abe9
cwchilvers <cwchilvers@gmail.com>
2023-04-19 20:45:50 -0700

    Elminated some uneccessary variables by combining them into an array and a matrix. Added a clear screen function and a function to create the game over screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,97 @@
 const body = document.body;
+
 // Created Elements
 const h1El = document.createElement("h1");
 const pEl = document.createElement("p");
 const buttonEl = document.createElement("button");
+
 // Header Text
-const h1E1TitleScreen = "Coding Quiz Challenge";
-const h1E1Q1 = "Commonly used data types do NOT include:";
-const h1E1Q2 = "The condition in an if/else statment is enclosed with _____.";
-const h1E1Q3 = "Arrays in Javascript can be used to store _____.";
-const h1E1Q4 = "String values must be enclosed within _____ when being assigned variables.";
-const h1E1Q5 = "A very userful tool used during development and debugging for printing content to the debugger is:";
+const h1E1Title = "Coding Quiz Challenge";
+const h1E1Questions = [
+    "Commonly used data types do NOT include:",
+    "The condition in an if/else statment is enclosed with _____.",
+    "Arrays in Javascript can be used to store _____.",
+    "String values must be enclosed within _____ when being assigned variables.",
+    "A very userful tool used during development and debugging for printing content to the debugger is:",
+]
 const h1E1GameOver = "All done!";
 const h1E1HighScores = "High Scores";
+
 // Paragraph Text
-const pElTitleScreen = "Try to answer the folowing code-related questions within the time limit. Keep in mind that wrong answers will penalize your score/time by 10 seconds!";
+const pElTitle = "Try to answer the folowing code-related questions within the time limit. Keep in mind that wrong answers will penalize your score/time by 10 seconds!";
+
 // Button Text
-const buttonElTitleScreen = "Start Quiz";
+const buttonElTitle = "Start Quiz";
+const buttonElAnswers = [
+    ["Strings","Booleans","Alerts","Numbers"],
+    ["Quotes","Curly Brackets","Parenthesis","Square Brackets"],
+    ["Numbers and Strings","Other Arrays","Booleans","All of the Above"],
+    ["Commas","Curly Brackets","Quotes","Parenthesis"],
+    ["Javascript","Terminal/Bash","For Loops","console.log"]
+];
+
+
+
+
+
+
+
+
 // Application Variables
-var quiz = true;
-var quizState = 0;
+var application = true;
+var appState = 0;
 var isLoaded = false;
 
-// Application Loop
-while (quiz === true) {
-    // Title Screen
-    if (quizState === 0 && isLoaded === false) {
-        TitleScreen();
-        isLoaded = true;
-    }
-    // Quiz
-    if (quizState === 1) {
+// Quiz Constants/Variables
+const questions = [1,2,3,4,5];
+var questionsLeft = [];
 
-    }
-    // Game Over
-    if (quizState === 2) {
 
-    }
-    // 
-    if (quizState === 3) {
 
-    }
+
+
+if (appState === 0) {
+    CreateTitle();
+    isLoaded = true;
 }
+// Quiz
+if (appState === 1) {
+
+}
+// Game Over
+if (appState === 2) {
+
+}
+// High Scores
+if (appState === 3) {
+
+}
+
+
+
+
+
+// Clear Screen
+function ClearScreen() {
+    body.innerHTML = '';
+}
+
+
+
+
 
 // Create Title Screen
-function TitleScreen() {
+function CreateTitle() {
     body.appendChild(h1El);
-    h1El.textContent = h1E1TitleScreen;
+    h1El.textContent = h1E1Title;
     body.appendChild(pEl);
-    pEl.textContent = pElTitleScreen;
+    pEl.textContent = pElTitle;
     body.appendChild(buttonEl);
-    buttonEl.textContent = buttonElTitleScreen;
+    buttonEl.textContent = buttonElTitle;
 }
 
-
+// Create Game Over
+function CreateGameOver() {
+    body.appendChild(h1El);
+    h1El.textContent = h1E1GameOver;
+}
