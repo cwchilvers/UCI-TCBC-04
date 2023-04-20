6039c94b79b8aeebd2d2c5d6905b70329f8a7954
cwchilvers <cwchilvers@gmail.com>
2023-04-19 21:14:51 -0700

    Created a function that selects a random question

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ const buttonEl = document.createElement("button");
 // Header Text
 const h1E1Title = "Coding Quiz Challenge";
 const h1E1Questions = [
-    "Commonly used data types do NOT include:",
-    "The condition in an if/else statment is enclosed with _____.",
-    "Arrays in Javascript can be used to store _____.",
-    "String values must be enclosed within _____ when being assigned variables.",
-    "A very userful tool used during development and debugging for printing content to the debugger is:",
+    "Commonly used data types do NOT include:",                                                             // Question 1
+    "The condition in an if/else statment is enclosed with _____.",                                         // Question 2
+    "Arrays in Javascript can be used to store _____.",                                                     // Question 3
+    "String values must be enclosed within _____ when being assigned variables.",                           // Question 4
+    "A very userful tool used during development and debugging for printing content to the debugger is:",   // Question 5
 ]
 const h1E1GameOver = "All done!";
 const h1E1HighScores = "High Scores";
@@ -23,11 +23,11 @@ const pElTitle = "Try to answer the folowing code-related questions within the t
 // Button Text
 const buttonElTitle = "Start Quiz";
 const buttonElAnswers = [
-    ["Strings","Booleans","Alerts","Numbers"],
-    ["Quotes","Curly Brackets","Parenthesis","Square Brackets"],
-    ["Numbers and Strings","Other Arrays","Booleans","All of the Above"],
-    ["Commas","Curly Brackets","Quotes","Parenthesis"],
-    ["Javascript","Terminal/Bash","For Loops","console.log"]
+    ["Strings","Booleans","Alerts","Numbers"],                              // Answers for Question 1
+    ["Quotes","Curly Brackets","Parenthesis","Square Brackets"],            // Answers for Question 2
+    ["Numbers and Strings","Other Arrays","Booleans","All of the Above"],   // Answers for Question 3
+    ["Commas","Curly Brackets","Quotes","Parenthesis"],                     // Answers for Question 4
+    ["Javascript","Terminal/Bash","For Loops","console.log"]                // Answers for Question 5
 ];
 
 
@@ -38,21 +38,20 @@ const buttonElAnswers = [
 
 
 // Application Variables
-var application = true;
 var appState = 0;
-var isLoaded = false;
 
 // Quiz Constants/Variables
 const questions = [1,2,3,4,5];
 var questionsLeft = [];
+var question;
 
 
 
 
 
 if (appState === 0) {
-    CreateTitle();
-    isLoaded = true;
+    Title();
+    questionsLeft = questions;
 }
 // Quiz
 if (appState === 1) {
@@ -60,28 +59,37 @@ if (appState === 1) {
 }
 // Game Over
 if (appState === 2) {
-
+    GameOver();
 }
 // High Scores
 if (appState === 3) {
-
+    HighScores();
 }
 
 
 
 
 
-// Clear Screen
-function ClearScreen() {
-    body.innerHTML = '';
+// Select Quesiton
+function SelectQuestion() {
+    question = questionsLeft[Math.floor(Math.random()*questionsLeft.length)];
+    // Remove selected question from pool
+    for (i = 0; i < questionsLeft.length; i++) {
+        if (questionsLeft[i] === question) {
+            questionsLeft.splice(i, 1);
+        }
+    }
 }
+
+
 
 
 
 
 
 // Create Title Screen
-function CreateTitle() {
+function Title() {
+    ClearScreen();
     body.appendChild(h1El);
     h1El.textContent = h1E1Title;
     body.appendChild(pEl);
@@ -91,7 +99,25 @@ function CreateTitle() {
 }
 
 // Create Game Over
-function CreateGameOver() {
+function GameOver() {
+    ClearScreen();
     body.appendChild(h1El);
     h1El.textContent = h1E1GameOver;
+}
+
+// Create High Scores
+function HighScores() {
+    ClearScreen();
+    body.appendChild(h1El);
+    h1El.textContent = h1E1HighScores;
+}
+
+
+
+
+
+
+// Clear Screen
+function ClearScreen() {
+    body.innerHTML = '';
 }
